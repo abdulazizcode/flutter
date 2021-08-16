77162b43a43273b5b75edb4b92c3261ef4813507
abdulazizcode <tajikcoder@gmail.com>
2021-08-17 05:58:40 +0800

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(
+        backgroundColor:Colors.teal,
+        body:SafeArea(
+            child: Column(
+               mainAxisAlignment:MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/aziz.png'),
+                ),
+                Text('Aziz Abdurasul',
+                style:TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color:Colors.white,
+                  fontWeight:FontWeight.bold,
+                ),
+                ),
+                Text('Flutter Developer',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal[100],
+                  fontSize:20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+
+                ),
+                SizedBox(
+                  height:20.0,
+                  width:150.0,
+                 child:Divider(
+                   color:Colors.teal.shade100,
+                 ),
+                ),
+                Card(
+                  color:Colors.white,
+                  margin:EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading:Icon(
+                      Icons.email,
+                      color:Colors.teal,
+                    ),
+                    title:Text(
+                      'tajikcoder@gmail.com',
+                      style:TextStyle(
+                        fontSize:20.0,
+                        color:Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color:Colors.white,
+                  margin:EdgeInsets.symmetric(vertical: 10.0, horizontal:25.0),
+                  child: ListTile(
+                    leading:Icon(
+                          Icons.phone,
+                        color:Colors.teal,
+                    ),
+                    title: Text('+60 1123 3753 37',
+                    style: TextStyle(
+                       color: Colors.teal.shade900,
+                       fontFamily:'Source Sans Pro',
+                       fontSize: 20.0,
+                     ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+    );
+  }
+}
+
+
+
